9103de171b200ab280df11352bc641003a3f82c1
ClaytonBudd <claytonbudd@gmail.com>
2023-09-05 15:40:27 -0600

    create index, script, style. build html calculator and math functions

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(a,b){
+    let result = a +b;
+    return result
+}
+
+function subtract(a,b){
+    let result = a - b;
+    return result
+}
+
+function multiply(a,b){
+    let result = a * b;
+    return result
+}
+
+function divide(a,b){
+    let result = a / b
+    return result
+}
+
+function operate(operator, a, b){
+    // takes in operator and 2 numbers and calls the proper function
+}
