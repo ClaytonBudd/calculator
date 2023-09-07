c45cd29c725a0e7137d336ef7f28582fd3249859
ClaytonBudd <claytonbudd@gmail.com>
2023-09-06 18:48:50 -0600

    completed operate func, cleaned up reply-operator to account for chaining commands, centered display css.

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
 let vars = {
-    num1: "", num2: "", operator: "", operatorStored: false, display: null
+    num1: "", num2: "", operator: "", secondVarStored: false, operatorStored: false, display: null
 };
 document.getElementById("box").innerHTML = vars.display;
 
 
 function updateDisplay(){
     document.getElementById("box").innerHTML = vars.display;
+    
 }
+
+
 
 function reply_numbers(clicked_object){
     if (vars.operatorStored === false){
@@ -17,6 +20,7 @@ function reply_numbers(clicked_object){
     }else{
         vars.num2 += clicked_object.getAttribute('data-other-attr');
         vars.display= vars.num2
+        vars.secondVarStored = true;
         updateDisplay();
     }
 
@@ -24,7 +28,7 @@ function reply_numbers(clicked_object){
 
 function resetVars(){
     vars = {
-        num1: "", num2: "", operator: "", operatorStored: false, display: null
+        num1: "", num2: "", operator: "", secondVarStored: false, operatorStored: false, display: null
     };
     updateDisplay();
     return vars
@@ -32,19 +36,26 @@ function resetVars(){
 
 function reply_operators(clicked_object){
     result = clicked_object.getAttribute('data-other-attr')
+        vars.operator = result    
         vars.operatorStored = true
-        vars.operator = result
-        vars.display = result
-        updateDisplay();
+        if (vars.secondVarStored == true){
+            operate(vars.operator, vars.num1, vars.num2)
+            
+            
+        }else{
+            vars.display = result
+            updateDisplay();
     }
+    
+}
 
 function add(a,b){
-    let result = a +b;
+    let result = Number(a) + Number(b);
     return result
 }
 
 function subtract(a,b){
-    let result = a - b;
+    let result = Number(a) - Number(b);
     return result
 }
 
@@ -59,15 +70,24 @@ function divide(a,b){
 }
 
 
-// event listener button push (numbers) save to num1, display
-// if button push again and is numbers concatenate to num 1, display
-// if button push again and is operator save to operator, display
-// if button push again and is number save to num2, display
-// concatenate number button push to num 2 until equals or clear is hit
-// if equals call operate function and display result
-// if clear clear all variables operators and display
-
-
 function operate(operator, a, b){
-    // takes in operator and 2 numbers and calls the proper function
+    let result
+    switch (vars.operator){
+        case "+":
+            result = add(a,b);
+            break;
+        case "-":
+            result = subtract(a,b);
+            break;
+        case "*":
+            result = multiply(a,b);
+            break;
+        case "/":
+            result = divide(a,b);
+            break;  
+        }
+    vars.num1 = result;
+    vars.num2 = ""
+    vars.display = result;
+    updateDisplay()
 }
